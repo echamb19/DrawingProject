bcc03674b6e017ecd70ef646e30431dc9321230b
Eden Chambers <EdenChambers5@gmail.com>
2019-05-03 13:16:55 -0600

    I added the Frame and the required stuff inside of it.

--- src/draw/view/DrawFrame.java
+++ src/draw/view/DrawFrame.java
@@ -1,6 +1,29 @@
 package draw.view;
 
-public class DrawFrame
-{
+import draw.controller.DrawController;
+import javax.swing.JFrame;
 
+public class DrawFrame extends JFrame 
+{
+	private DrawController app; 
+	private DrawPanel panel; 
+	
+	public DrawFrame(DrawController app)
+	{
+		super(); 
+		
+		this.app = app; 
+		this.panel = panel; 
+		
+		setupFrame(); 
+	}
+	
+	private void setupFrame()
+	{
+//		this.setContentPane(panel); 
+		this.setTitle("Draw GUI"); 
+		this.setSize(1000, 800); 
+		this.setResizable(false); 
+		this.setVisible(true); 
+	}
 }
