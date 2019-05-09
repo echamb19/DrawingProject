73d397724e5e1adebf77655fa235c9537ad488f9
Eden Chambers <EdenChambers5@gmail.com>
2019-05-09 13:21:57 -0600

    I added the import methods in the ArtPanel.

--- src/draw/view/ArtPanel.java
+++ src/draw/view/ArtPanel.java
@@ -1,7 +1,23 @@
 package draw.view;
 
-import javax.swing.JPanel; 
 import draw.controller.DrawController;
+import javax.swing.JPanel; 
+
+import java.awt.BasicStroke;
+import java.awt.Color; 
+import java.awt.Dimension; 
+import java.awt.Graphics; 
+import java.awt.Graphics2D; 
+import java.awt.image.BufferedImage; 
+
+import java.io.File; 
+import java.io.IOException; 
+
+import javax.imageio.ImageIO; 
+import javax.swing.JFileChooser; 
+import javax.swing.filechooser.FileFilter; 
+import javax.swing.filechooser.FileNameExtensionFilter; 
+
 
 public class ArtPanel extends JPanel
 {
